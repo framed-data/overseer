a004a39325e5de80e3356644f8b26a1c545a6aaf
Andrew Berls <andrew.berls@gmail.com>
2015-02-02 23:28:42 +0000

    Cut Overseer 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.framed/overseer "0.1.0"
+(defproject io.framed/overseer "0.2.0"
   :description "A framework for defining and running expressive job pipelines"
   :url "https://github.com/framed-data/overseer"
   :license {:name "MIT License"
