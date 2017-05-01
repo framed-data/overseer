1d335fb32621489071798a5c2e65df857578bfc4
Elliot Block <eblock@squareup.com>
2017-05-01 16:48:24 -0700

    Add dependencies on loom, JDBC and H2

--- project.clj
+++ project.clj
@@ -16,7 +16,10 @@
                  [com.datomic/datomic-free "0.9.5130" :exclusions [joda-time]]
                  [clj-http "1.1.2"]
                  [raven-clj "1.1.0"]
-                 [io.framed/std "0.1.2"]]
+                 [io.framed/std "0.1.2"]
+                 [aysylu/loom "1.0.0"]
+                 [org.clojure/java.jdbc "0.7.0-alpha3"]
+                 [com.h2database/h2 "1.4.195"]]
   :aot [overseer.runner]
   :plugins [[codox "0.8.13"]]
   :codox {:output-dir "doc/api"})
