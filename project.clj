275662121d2e511809afc67f0ffabe434e5ee8ac
Andrew Berls <andrew.berls@gmail.com>
2019-02-17 15:25:20 -0800

    Upgrade dependencies, release version 0.8.9
    https://github.com/framed-data/overseer/issues/101
    
    Overseer 0.8.8 relied on a rather old version of `clj-http` which caused
    compile errors on Clojure 1.10.0 (via a dependency on `potemkin`). This
    upgrades and trims Overseer's dependencies as follows:
    
    - Upgrade Clojure to 1.8.0
    - Upgrade `raven-clj` to `1.5.2` (this now transitively pulls in
      clj-http 3.0.1 and we no longer need to specify a bound as in [0])
    - Remove unused `clj-yaml`, `data.fressian` and `tools.cli` dependencies
    
    0:
    https://github.com/framed-data/overseer/commit/1dd1b494fe70208c4c51b48e627dafe35119bc5d

--- project.clj
+++ project.clj
@@ -7,15 +7,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/framed-data/overseer"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [circleci/clj-yaml "0.5.3"]
-                 [org.clojure/data.fressian "0.2.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-json "0.5.3"]
-                 [org.clojure/tools.cli "0.3.1"]
                  [com.taoensso/timbre "4.0.2"]
                  [com.datomic/datomic-free "0.9.5130" :exclusions [joda-time]]
-                 [clj-http "1.1.2"]
-                 [raven-clj "1.1.0"]
+                 [raven-clj "1.5.2"]
                  [io.framed/std "0.2.5"]
                  [aysylu/loom "1.0.0"]
                  [org.clojure/java.jdbc "0.7.0-alpha3"]
